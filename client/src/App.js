eeefc13eef63d67041ed1f363715edfa5a7ff355
edrylle123 <mr.edrylleramos@gmail.com>
2023-11-06 13:25:27 +0800

    updated with qrcode

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Landing from "./Landing";
+// import Admin from "./Signup";
+import Password from "./Password";
+import Signup from "./Signup";
+import Registration from "./Registration";
+import Qrc from "./Qrc";
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* <Route path="/" element={<Landing />} /> */}
+          <Route path="/" element={<Signup />} />
+          <Route path="/admin" element={<Password />} />
+          <Route path="/login" element={<Signup />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/qrcode" element={<Qrc />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/Signup.js
@@ -0,0 +1,158 @@
+import "./admin.css";
+import { Link } from "react-router-dom";
+import { Card } from "./Components/Card";
+import image1 from "./Components/one.PNG";
+import image2 from "./Components/NUMBERING/2.png";
+import image3 from "./Components/NUMBERING/3.png";
+import image4 from "./Components/NUMBERING/4.png";
+import image5 from "./Components/NUMBERING/5.png";
+import image6 from "./Components/NUMBERING/6.png";
+import image7 from "./Components/NUMBERING/7.png";
+import image8 from "./Components/NUMBERING/8.png";
+import image9 from "./Components/NUMBERING/9.png";
+import image10 from "./Components/NUMBERING/10.png";
+import image11 from "./Components/NUMBERING/11.png";
+import image12 from "./Components/NUMBERING/12.png";
+import image13 from "./Components/NUMBERING/13.png";
+import image14 from "./Components/NUMBERING/14.png";
+import image15 from "./Components/NUMBERING/15.png";
+import image16 from "./Components/NUMBERING/16.png";
+import image17 from "./Components/NUMBERING/17.png";
+import image18 from "./Components/NUMBERING/18.png";
+import image19 from "./Components/NUMBERING/19.png";
+import image20 from "./Components/NUMBERING/20.png";
+import image21 from "./Components/NUMBERING/21.png";
+import image22 from "./Components/NUMBERING/22.png";
+import image23 from "./Components/NUMBERING/23.png";
+import image24 from "./Components/NUMBERING/24.png";
+
+function Signup() {
+  return (
+    <div className="App">
+      <div className="card-grid">
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image1} imgAlt="PC 1" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image2} imgAlt="PC 2" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image3} imgAlt="PC 3" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image4} imgAlt="PC 4" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image5} imgAlt="PC 5" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image6} imgAlt="PC 6" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image7} imgAlt="PC 7" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image8} imgAlt="PC 8" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image9} imgAlt="PC 9" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image10} imgAlt="PC 10" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image11} imgAlt="PC 11" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image12} imgAlt="PC 12" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image13} imgAlt="PC 13" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image14} imgAlt="PC 14" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image15} imgAlt="PC 15" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image16} imgAlt="PC 16" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image17} imgAlt="PC 17" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image18} imgAlt="PC 18" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image19} imgAlt="PC 19" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image20} imgAlt="PC 20" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image21} imgAlt="PC 21" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image22} imgAlt="PC 22" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image23} imgAlt="PC 23" buttonText="LOGIN" />
+          </Link>
+        </div>
+        <div className="col">
+          <Link to="/landing">
+            <Card imgSrc={image24} imgAlt="PC 24" buttonText="LOGIN" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
